7d83c198e76cce7d65ce7ab9deb44ac596e11dfb
Camilo Parra Mariño <ccparram@unal.edu.co>
2017-02-09 22:25:14 -0500

    Add css & js files in header template

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,7 +1,21 @@
 <html>
         <head>
                 <title>CodeIgniter Tutorial</title>
+
+                <?php
+                if (isset($css)){
+                        foreach ($css as $_css) {
+                         echo  '<link href="' . $_css . '" rel="stylesheet">';
+                         echo "\n\t\t\t\t";
+                        }
+                }
+                if (isset($js)){
+                        foreach ($js as $_js) {
+                         echo  '<script src="' . $_js . '"></script>';
+                         echo "\n\t\t\t\t";
+                        }
+                }
+                ?>
         </head>
         <body>
-
-                <h1><?php echo $title; ?></h1>
+                
