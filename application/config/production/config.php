eac9880d6aa39e6df7ff56a0985c46ec90e7ab62
Camilo Parra Mariño <ccparram@unal.edu.co>
2017-03-26 00:41:23 -0500

    Refactor base_url for work in Heroku

--- application/config/production/config.php
+++ application/config/production/config.php
@@ -1,4 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$config['base_url'] = 'https://evening-sierra-26626.herokuapp.com/';
+$config['base_url'] = 'https://evening-sierra-26626.herokuapp.com/index.php';
+
+$config['index_page'] = '';
